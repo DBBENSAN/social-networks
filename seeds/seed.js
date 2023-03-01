91a24003900b36e965024dfa0f2cceaa861a0690
DBENSAN <dftb97@gmail.com>
2023-03-01 00:19:49 -0800

    fix mongo connection

--- seeds/seed.js
+++ seeds/seed.js
@@ -6,6 +6,12 @@ const db = require('../config/connection')
 const seedDatabase = async () => {
    db.on('error', console.error.bind(console, 'MongoDB connection error:'));
    db.once('open', async () => {
+      console.log(`
+      .............................
+      ........Database seeding.....
+      .............................
+      
+      `)
       await seedUsers();
       await seedThoughts()
       console.log('DB SEEDED DB SEEDED DB SEEDED DB SEEDED DB SEEDED')
